f95069712fe00e3333229ae19c3d03237f5c6e20
Trankanh <kimanh79@gmail.com>
2018-12-21 16:26:24 +0700

    Bai 3
    Bai 3

---
+++ baitap3.php
@@ -0,0 +1,41 @@
+<?php
+     function loai_bo($mang)
+    {
+    	//$mang_can_ktra = array(a,e,i, o,u,y);
+        $x = count($mang);
+        $mang_moi = array();
+        //$y = count($mang_can_ktra);
+        for($i = 0; $i < $x; $i++)
+        {	
+        	$strlen = strlen($mang[$i]);
+        	$chuoi = '';
+           for($j = 0; $j < $strlen; $j++)
+           {
+           		
+           		$char = substr($mang[$i],$j,1);
+           		if(preg_match('/[aeiouy]/', $char))
+           		{
+           			$char = strtoupper($char);
+           		}
+           		$chuoi .= $char;
+           }
+           array_push($mang_moi,$chuoi);
+        }
+        return $mang_moi;
+          
+        
+    }
+    function in_mang($mang)
+    {
+    	
+        foreach ($mang as $value) 
+        {
+             echo $value.'-----';
+        }   
+    }
+    $ds = array('aaa','kanh io');
+ 	in_mang(loai_bo($ds));
+
+
+
+?>
