36c938064e2e867ef3705fdf74a2f3555591d74c
Trankanh <kimanh79@gmail.com>
2018-12-21 16:30:13 +0700

    Bai 2
    Bai 2

---
+++ bai2.php
@@ -0,0 +1,29 @@
+<?php
+	 function loai_bo($mang)
+    {
+    	$mang_moi = [];
+        $x = count($mang);
+        for ($i = 0; $i < $x; $i++)
+        {
+            if(!in_array($mang[$i],$mang_moi))
+            {
+             	array_push($mang_moi,$mang[$i]);
+            }
+              
+             
+        }
+        return $mang_moi;
+        
+    }
+     function in_mang($mang)
+    {
+    	
+        foreach ($mang as $value) 
+        {
+             echo $value.'-----';
+        }   
+    }
+    $ds = array(1,6,8,2,5,14,3,6);
+ 	in_mang(loai_bo($ds));
+
+?>
